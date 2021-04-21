a297e6f8a849a0cd5d36583f0f0c69b5e46be64f
Swapnil <swapnil.renge@geospoc.com>
2021-04-21 22:45:26 +0530

    refactor: created reducers as seperate file

--- src/components/MusicPlayer/index.js
+++ src/components/MusicPlayer/index.js
@@ -5,72 +5,9 @@ import Topbar from './Topbar'
 import Sidebar from './Sidebar'
 import Content from './Content'
 import Playbar from './Playbar'
-import media from '../../media.json'
+import { initialState, reducer } from '../../reducers'
 
 export const StoreContext = createContext(null)
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_PLAYLIST':
-      return {
-        ...state,
-        playlists: { ...state.playlists, [action.playlist]: new Set() }
-      }
-    case 'SET_PLAYLIST':
-      return {
-        ...state,
-        currentPlaylist: action.playlist
-      }
-    case 'ADD_FAVORITE':
-      state.playlists.favorites.add(action.songId)
-      return { ...state }
-    case 'REMOVE_FAVORITE':
-      state.playlists.favorites.delete(action.songId)
-      return { ...state }
-    case 'SET_CURRENT_SONG':
-      return { ...state, currentSongId: action.songId }
-    case 'PLAY':
-      return {
-        ...state,
-        currentSongId: action.songId || state.currentSongId,
-        playing: true
-      }
-    case 'PAUSE':
-      return {
-        ...state,
-        playing: false
-      }
-    case 'SET_VOLUME':
-      return {
-        ...state,
-        currentVolume: action.value
-      }
-    case 'SET_CURRENT_TIME':
-      return {
-        ...state,
-        currentTime: action.time
-      }
-    case 'SET_CURRENT_DURATION':
-      return {
-        ...state,
-        currentDuration: action.duration
-      }
-  }
-  return state
-}
-const DEFAULT_PLAYLIST = 'home'
-const initialState = {
-  currentPlaylist: DEFAULT_PLAYLIST,
-  playlists: {
-    home: new Set(media.ids),
-    favorites: new Set()
-  },
-  media,
-  playing: false,
-  currentSongId: null,
-  currentVolume: 0.5,
-  currentTime: 0,
-  currentDuration: 0
-}
 
 const MusicPlayer = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -80,7 +17,7 @@ const MusicPlayer = () => {
         <Topbar></Topbar>
         <Sidebar />
         <Content />
-        <Playbar></Playbar>
+        <Playbar />
       </div>
     </StoreContext.Provider>
   )
